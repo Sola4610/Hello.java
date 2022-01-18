d576375988969a78e9581a644b14443084f5abb5
Sola4610 <97980070+Sola4610@users.noreply.github.com>
2022-01-18 13:18:01 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/******************************************************
+*Java Program Name
+*******************************************************
+*Solaris Serrano
+*1/18/22
+*CMSC 255-090
+*******************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Project Description>");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Section Number>");			
+		System.out.println();
+	}
+ }     
